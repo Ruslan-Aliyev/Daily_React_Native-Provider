127f328843114cc438cbfc65371a30c1dabb5dd9
Rus <rus@Russ-MacBook-Air.local>
2025-02-10 12:50:15 +0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Daily from '@daily-co/react-native-daily-js';
 
 export default function App() {
+  let call = null;
+
+  if (!call)
+  {
+    call = Daily.createCallObject();
+    call.setLocalVideo(false);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>RN Daily</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={() => {
+          call.join({ url: 'https://ruslanaliyev.daily.co/ruslanaliyev_room1', startVideoOff: true });
+        }}
+        title="Call"
+      />
+      <Button
+        onPress={() => {
+          call.leave();//Need await?
+          call.destroy();//Need await?
+        }}
+        title="Hangup"
+      />
     </View>
   );
 }
